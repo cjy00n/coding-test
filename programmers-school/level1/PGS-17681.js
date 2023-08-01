4ef5fdd61896d74481b60b163ab8adb8ff0dc6a5
cjy921004 <ej921004@naver.com>
2023-08-02 02:21:47 +0900

    [PGS] #17681 : [1차]비밀지도 (카카오2018)

---
+++ programmers-school/level1/PGS-17681.js
@@ -0,0 +1,30 @@
+/*
+function solution(n, arr1, arr2) {
+  var answer = [];
+  for (let i = 0; i < n; i++) {
+    let s1 = arr1[i].toString(2);
+    let s2 = arr2[i].toString(2);
+    s1 = [...("0".repeat(n - s1.length) + s1)];
+    s2 = [...("0".repeat(n - s2.length) + s2)];
+    let str = "";
+    for (let j = 0; j < n; j++) {
+      if (s1[j] == 1 || s2[j] == 1) str += "#";
+      else str += " ";
+    }
+    answer.push(str);
+  }
+  return answer;
+}
+*/
+function solution(n, arr1, arr2) {
+  return arr1.map((v, i) =>
+    addZero(n, (v | arr2[i]).toString(2)).replace(/1|0/g, (a) =>
+      +a ? "#" : " "
+    )
+  );
+}
+
+const addZero = (n, s) => {
+  return "0".repeat(n - s.length) + s;
+};
+console.log(solution(6, [46, 33, 33, 22, 31, 50], [27, 56, 19, 14, 14, 10]));
