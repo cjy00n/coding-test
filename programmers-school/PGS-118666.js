6f618035e59b05b39c6be517a8f5476eeda75ac8
최정윤 <ej921004@naver.com>
2024-01-12 17:52:32 +0900

    [PGS] #118666 : 성격 유형 검사하기 (2022 KAKAO TECH INTERNSHIP)

---
+++ programmers-school/PGS-118666.js
@@ -0,0 +1,30 @@
+// 성격 유형 검사하기(2022 KAKAO TECH INTERNSHIP)
+// https://school.programmers.co.kr/learn/courses/30/lessons/118666
+function solution(survey, choices) {
+  const MBTI = {};
+  const types = ["RT", "CF", "JM", "AN"];
+
+  types.forEach((type) => type.split("").forEach((char) => (MBTI[char] = 0)));
+
+  choices.forEach((choice, idx) => {
+    const [disagree, agree] = survey[idx];
+    MBTI[choice < 4 ? disagree : agree] += Math.abs(choice - 4);
+  });
+
+  return types.map(([a, b]) => (MBTI[a] >= MBTI[b] ? a : b)).join("");
+}
+
+function mySolution(survey, choices) {
+  const MBTI = { R: 0, T: 0, C: 0, F: 0, J: 0, M: 0, A: 0, N: 0 };
+  const types = ["RT", "CF", "JM", "AN"];
+
+  choices.forEach((choice, idx) => {
+    const [disagree, agree] = survey[idx];
+
+    MBTI[choice < 4 ? disagree : agree] += Math.abs(choice - 4);
+  });
+
+  return types.map(([a, b]) => (MBTI[a] >= MBTI[b] ? a : b)).join("");
+}
+
+console.log(solution(["AN", "CF", "MJ", "RT", "NA"], [5, 3, 2, 7, 5]));
