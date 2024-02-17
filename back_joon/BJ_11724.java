d86202a2ad9983bdfc40d7e24bcd7c6f04b9c490
최정윤 <ej921004@naver.com>
2024-02-18 01:50:27 +0900

    [BJ] #11724 : DFS > 연결 요소의 개수

--- back_joon/BJ_11724.java
+++ back_joon/BJ_11724.java
@@ -1,2 +1,57 @@
-package back_joon;public class BJ_11724 {
+// 연결 요소의 개수
+// https://www.acmicpc.net/problem/11724
+
+package back_joon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class BJ_11724 {
+    static boolean visited[];
+    static ArrayList<Integer>[] A;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        visited = new boolean[n + 1];
+        A = new ArrayList[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            A[i] = new ArrayList<Integer>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s = Integer.parseInt(st.nextToken()); // 시작점
+            int e = Integer.parseInt(st.nextToken()); // 종료점
+            A[s].add(e);
+            A[e].add(s);
+        }
+
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                count++;
+                DFS(i);
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    private static void DFS(int v) {
+        if (visited[v]) return;
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (!visited[i]) {
+                DFS(i);
+            }
+        }
+    }
 }
