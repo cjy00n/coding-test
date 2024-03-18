8311250eb086d54d801b9123d7f70eb9aa763713
cjy00n <ej921004@naver.com>
2024-03-18 18:43:54 +0900

    [PGS] #178871 : 달리기 경주

--- programmers-school/level1/PGS_178871.java
+++ programmers-school/level1/PGS_178871.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class PGS_178871 {
+public class PGS_178871 {
+    class Solution {
+        public String[] solution(String[] players, String[] callings) {
+            String[] answer = {};
+            return answer;
+        }
+    }
+    public static void main(String[] args) {
+
+    }
 }
