863a43b58e5f17dec0ae7871bb8e41c03d522673
cjy00n <ej921004@naver.com>
2024-04-12 04:25:08 +0900

    [BJ] #10926 : ??

--- back_joon/BJ_10926.java
+++ back_joon/BJ_10926.java
@@ -1,2 +1,35 @@
-package back_joon;public class BJ_10926 {
+package back_joon;
+
+import java.util.*;
+
+public class BJ_10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        sc.nextLine();
+
+        int cnt = N;
+
+        for (int i = 0; i < N; i++) {
+            String tmp = sc.nextLine();
+            Set<Character> set = new HashSet<Character>();
+
+            for (int j = 0; j < tmp.length(); j++) {
+                char ch = tmp.charAt(j);
+                if (set.contains(ch) && j > 0 && tmp.charAt(j - 1) != ch) {
+                    cnt--;
+                    break;
+                } else {
+                    set.add(ch);
+                }
+            }
+
+        }
+
+        sc.close();
+
+        System.out.println(cnt);
+
+
+    }
 }
